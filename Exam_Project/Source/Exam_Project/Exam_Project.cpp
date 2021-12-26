45c17ad47f7c1e3dc54edf2b2c7eed187285e368
bong-seob-shin <56147393+bong-seob-shin@users.noreply.github.com>
2021-12-26 22:39:04 +0900

    set preference

---
+++ Exam_Project/Source/Exam_Project/Exam_Project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Exam_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Exam_Project, "Exam_Project" );
---
+++ Exam_Project/Source/Exam_Project/Exam_ProjectGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "Exam_ProjectGameModeBase.h"
+
